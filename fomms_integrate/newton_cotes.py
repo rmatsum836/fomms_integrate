d89844701afadce01e9614d2cc9db389dd9c4f3e
Ray A. Matsumoto <ray.a.matsumoto@vanderbilt.edu>
2018-07-19 15:19:28 -0500

    add 'trapz function to 'newton_cotes.py'

--- fomms_integrate/newton_cotes.py
+++ fomms_integrate/newton_cotes.py
@@ -29,6 +29,34 @@ def canvas(with_attribution=True):
     return quote
 
 
+ """
+This file contains the implementation of the trapezoidal rule
+"""
+
+import numpy as np
+
+def trapz(x, f):
+    """ 
+    Compute a 1D definite integral using the trapezoidal rule
+    Parameters
+    ----------
+    f : function
+        User defined function.
+    x : numpy array
+        Integration domain.
+    Returns
+    -------
+    I : float
+        Integration result.
+    """   
+    a = x[0]
+    b = x[1]
+    ya = f(a)
+    yb = f(b)
+    I = (b-a) * (ya + yb) / 2
+    return I
+
+
 if __name__ == "__main__":
     # Do something if this file is invoked on its own
     print(canvas())
